9c965b7fe0856cd4c7b19831813cb242352597ea
Dave Lubawski <77742798+DingerDave@users.noreply.github.com>
2024-12-13 16:38:25 -0500

    add a TODO list, some data familization in testing_notebook and data preproccesing in data_preprocessing_utils

--- data_preprocessing_utils.py
+++ data_preprocessing_utils.py
@@ -22,4 +22,22 @@ def prep_voter_dataframe(voter_data: pd.DataFrame, drop_null: bool = True, shuff
     return voter_data
 
 def prep_housing_dataframe(housing_data: pd.DataFrame, drop_null: bool=True, shuffle: bool=False) -> pd.DataFrame:
-    pass
+    """
+    Prepares a housing dataframe by selecting specific columns, handling missing values,
+    formatting the tract_code column, and optionally shuffling the data.
+
+    Args:
+        housing_data (pd.DataFrame): Input housing data.
+        drop_null (bool, optional): If True, drops rows with null values. Defaults to True.
+        shuffle (bool, optional): If True, shuffles the dataframe. Defaults to False.
+
+    Returns:
+        pd.DataFrame: The processed voter dataframe.
+    """
+    housing_data = housing_data[['census_tract', 'derived_race', 'derived_sex', 'denial_reason-1']]
+    housing_data = housing_data.dropna() if drop_null else housing_data
+    housing_data["census_tract"] = housing_data["census_tract"].astype(int).astype(str)
+    housing_data["census_tract"] = housing_data["census_tract"].str.zfill(6)
+    if shuffle:
+        housing_data = housing_data.sample(frac=1).reset_index(drop=True)
+    return housing_data
